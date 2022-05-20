3c745227936dfa6b792da7c7385f0f0144a3e74d
sai <saik@gmail.com>
2022-05-21 00:42:55 +0530

    dsf

--- kubernetes.sh
+++ kubernetes.sh
@@ -2,6 +2,22 @@
 
 cd infra/k8s
 
+
+kubectl delete -A ValidatingWebhookConfiguration ingress-nginx-admission
+
+kubectl apply -f ingress-srv-prod.yaml --record=true
+sleep 5
+kubectl apply -f ingress-srv-prod.yaml
+#sleep 15
+
+kubectl apply -f ingress-srv-prod.yaml
+#sleep 15
+
+export theIPaddress=$(ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
+
+kubectl patch svc ingress-nginx-controller   -n ingress-nginx -p '{"spec": {"type": "LoadBalancer", "externalIPs":["'"$theIPaddress"'"]}}'
+
+
 kubectl apply -f storageclass.yml 
 
 kubectl apply -f auth-mongo.yaml
@@ -29,3 +45,4 @@ kubectl rollout restart deployment/orders-deployment
 kubectl rollout restart deployment/expiration-deployment
 kubectl rollout restart deployment/payments-deployment
 kubectl rollout restart deployment/client-deployment
+
