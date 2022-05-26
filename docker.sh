994db17a57806f43c72f4f1031aff0232d435b86
sai <saik@gmail.com>
2022-05-26 22:39:48 +0530

    sdfsdf

--- docker.sh
+++ docker.sh
@@ -2,7 +2,7 @@ docker login -u yashwanthjavvaji -p Password@
 
 echo 'auth'
 cd auth
-docker build -t yashwanthjavvaji/auth .
+docker build --network host -t yashwanthjavvaji/auth .
 docker push yashwanthjavvaji/auth
 cd ..
 
@@ -10,34 +10,34 @@ cd ..
 echo 'products'
 
 cd products
-docker build -t yashwanthjavvaji/products .
+docker build --network host -t yashwanthjavvaji/products .
 docker push yashwanthjavvaji/products
 cd ..
 
 
 echo 'orders'
 cd orders
-docker build -t yashwanthjavvaji/orders .
+docker build --network host -t yashwanthjavvaji/orders .
 docker push yashwanthjavvaji/orders
 cd ..
 
 
 echo 'expiration'
 cd expiration
-docker build -t yashwanthjavvaji/expiration .
+docker build --network host -t yashwanthjavvaji/expiration .
 docker push yashwanthjavvaji/expiration
 cd ..
 
 
 echo 'payments'
 cd payments
-docker build -t yashwanthjavvaji/payments .
+docker build --network host -t yashwanthjavvaji/payments .
 docker push yashwanthjavvaji/payments
 cd ..
 
 
 echo 'client'
 cd client
-docker build -t yashwanthjavvaji/client .
+docker build --network host -t yashwanthjavvaji/client .
 docker push yashwanthjavvaji/client
 cd ..
